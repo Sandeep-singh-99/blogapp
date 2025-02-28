901ae4d3657f09e0b6f69049fd88cdb55b3bc5f1
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-02-28 22:02:24 +0530

    Enhance AdminLogin component: add login form with email and password fields for improved user authentication

--- src/app/(v3)/admin-dashboard/admin-login/page.tsx
+++ src/app/(v3)/admin-dashboard/admin-login/page.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function AdminLogin() {
   return (
-    <div>AdminLogin</div>
+    <div>
+      <div>
+        <h1>Admin Login</h1>
+        <form>
+          <div>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" />
+          </div>
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
